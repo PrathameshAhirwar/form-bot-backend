2baefe503e3ce2fae8b6b7b81145167997ea3875
Prathamesh Ahirwar <prathameshahirwar2000@gmail.com>
2025-01-03 23:54:16 +0530

    db

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose')
 
 const connectDB = async ()=>{
     try{
-        await mongoose.connect(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI,
+            {useNewUrlParser: true,
+            useUnifiedTopology: true,
+            connectTimeoutMS: 30000, // 30 seconds
+            socketTimeoutMS: 30000,  // 30 seconds
+            }
+        )
     }catch(err){
         console.log(err)
     }
